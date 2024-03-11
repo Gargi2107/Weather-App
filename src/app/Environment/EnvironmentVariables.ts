bd7ba512b4b5dad8f553e308f97a3e4874d62abc
Gargi Nigam <gargi.nigam.ece21@itbhu.ac.in>
2024-03-11 21:10:56 +0530

    Your commit message here

---
+++ src/app/Environment/EnvironmentVariables.ts
@@ -0,0 +1,16 @@
+export const EnvironmentalVariables = {
+    production:false,
+
+    //Base Urls to access the end points using the http client get method
+    weatherApiLocationBaseURL:'https://weather338.p.rapidapi.com/locations/search?',
+    weatherApiForecastBaseURL:'https://weather338.p.rapidapi.com/weather/forecast?',
+    
+    //Variables for API key name and value
+    xRapidApiKeyName:'X-RapidAPI-Key',
+    xRapidApikeyValue:'1214116092mshb360f39ac58077ep1f9e2djsn13542ece6a7a',
+
+    //Variables for host name and value
+    xRapidApiHostName:'X-RapidAPI-Host',
+    xRapidApiHostValue:'weather338.p.rapidapi.com'
+    
+}
